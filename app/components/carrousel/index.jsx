115a95f524198e026727986f40bc6657b224be13
EAxelGMExcite <axel@excite.com.mx>
2023-03-17 11:22:09 -0600

    imagen de carrusel

--- app/components/carrousel/index.jsx
+++ app/components/carrousel/index.jsx
@@ -2,12 +2,23 @@ const Carrousel = () => {
   return (
     <div className="text-white">
       <div className="flex flex-col md:flex-row gap-1">
-        <div className="basis-1/2 bg-yellow-700">IMAGEN PRINCIPAL</div>
-        <div className="basis-1/2 flex flex-col gap-1 bg-fuchsia-700">
-          <div className="basis-1/2 bg-green-900">Segunda imagen</div>
-          <div className="basis 1/2 bg-rose-900 flex flex-col md:flex-row gap-1">
-            <div className="basis-1/2 bg-cyan-900">Tercer Imagen</div>
-            <div className="basis-1/2 border-indigo-800">Cuarta Imagen</div>
+        <div className="basis-1/2">
+          <div className="h-[30rem] flex flex-col justify-end" style={{ backgroundImage: "url('https://mexicotravelchannel.com.mx/wp-content/uploads/2023/03/imagen-destacada-wordpress-89-534x462.png')", backgroundSize: "cover" }}>
+            <div className="text-[27px] font-bold bg-primary/75 px-3">Super Plan en Desfile Artesanal en CDMX con mojigangas y todo</div>
+            <div className="bg-[#000000bb] px-2 text-[12px] py-1 w-fit">Axel Gonzalez - 04/02/2000</div>
+          </div>
+        </div>
+        <div className="basis-1/2 flex flex-col gap-1">
+          <div className="basis-1/2 h-[15rem] flex flex-col justify-end" style={{ backgroundImage: "url('https://mexicotravelchannel.com.mx/wp-content/uploads/2023/03/imagen-destacada-wordpress-89-534x462.png')", backgroundSize: "cover" }}>
+            <div className="text-[20px] font-bold bg-primary/75 px-3">Super Plan en Desfile Artesanal en CDMX con mojigangas y todo</div>
+          </div>
+          <div className="basis 1/2 h-[15rem] flex flex-col md:flex-row gap-1">
+            <div className="basis-1/2 flex flex-col justify-end" style={{ backgroundImage: "url('https://mexicotravelchannel.com.mx/wp-content/uploads/2023/03/imagen-destacada-wordpress-89-534x462.png')", backgroundSize: "cover" }}>
+              <div className="text-[15px] font-bold bg-primary/75 px-3">Super Plan en Desfile Artesanal en CDMX con mojigangas y todo</div>
+            </div>
+            <div className="basis-1/2 flex flex-col justify-end" style={{ backgroundImage: "url('https://mexicotravelchannel.com.mx/wp-content/uploads/2023/03/imagen-destacada-wordpress-89-534x462.png')", backgroundSize: "cover" }}>
+              <div className="text-[15px] font-bold bg-primary/75 px-3">Super Plan en Desfile Artesanal en CDMX con mojigangas y todo</div>
+            </div>
           </div>
         </div>
       </div>
