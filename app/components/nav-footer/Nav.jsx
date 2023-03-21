655bf0bee54416132041049a690319a23a3d9c3c
EAxelGMExcite <axel@excite.com.mx>
2023-03-21 16:19:04 -0600

    borrar comentados

--- app/components/nav-footer/Nav.jsx
+++ app/components/nav-footer/Nav.jsx
@@ -133,48 +133,6 @@ const Nav = ({ navigation = [] }) => {
                 </div>
               </div>
             </div>
-            {/* <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-              <div className="flex flex-row h-16 items-center justify-between">
-                <div className="flex items-center">
-                  <div className="flex-shrink-0">
-                    <Link to="/">
-                      <img className="lg:w-36 w-14" src="/icon.svg" alt="Mexico Travel Channel" />
-                    </Link>
-                  </div>
-                  <div className="hidden lg:block">
-                    <div className="ml-10 divide-y divide-solid space-y-4">
-                      <div className="flex flex-row-reverse gap-3">
-                        {social_medias.map((social, index) => (
-                          <div key={index} className="text-white">
-                            <a href={social.href} target="_blank" rel="noopener nofollow noreferrer">
-                              {social.icon}
-                            </a>
-                          </div>
-                        ))}
-                      </div>
-                      <div className="flex items-baseline space-x-4 pt-5">
-                        {navigation.map((item, index) => (
-                          <Link key={index} to={item.href} className={classNames(item.href === location.pathname ? "bg-red-900 text-white" : "text-gray-300 hover:bg-red-400 hover:text-white", "rounded-md px-3 py-2 text-sm font-medium")}>
-                            {item.name}
-                          </Link>
-                        ))}
-                        <Link to="/en-vivo" className={classNames("/en-vivo" === location.pathname ? "bg-red-900 text-white" : "text-gray-300 hover:bg-red-400 hover:text-white", "rounded-md px-3 py-2 text-sm font-medium")}>
-                          En vivo
-                        </Link>
-                      </div>
-                    </div>
-                  </div>
-                </div>
-                <div className="mr-2 flex lg:hidden">
-                   Mobile menu button 
-                  <button className="inline-flex items-center justify-center rounded-md bg-primary p-2 text-gray-400 hover:bg-red-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-primary" onClick={() => setOpenModal(!openModal)}>
-                    <span className="sr-only">Open main menu</span>
-                    {openModal ? <XMarkIcon className="block h-6 w-6" aria-hidden="true" /> : <Bars3Icon className="block h-6 w-6" aria-hidden="true" />}
-                  </button>
-                </div>
-              </div>
-            </div> */}
-
             <DialogMaterial fullScreen open={openModal} onClick={() => setOpenModal(!openModal)} TransitionComponent={Transition2} className="lg:hidden">
               <AppBar sx={{ position: "relative" }}>
                 <Toolbar>
